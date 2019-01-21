e8e64a3e44ac878dce000af1e03367edc80bf4a4
Dinuksha <dinuksha@wso2.com>
2019-01-21 11:48:45 +0530

    Change alerts and validation

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+var app = angular.module('urlApp', []);
+
+app.controller('urlValidator', function($scope) {
+
+    $scope.urlCheckvalid = true;
+    $scope.urlCreateValid = true;
+
+    var url = "amqp://"+$scope.username+":"+$scope.pwd+"@clientid/carbon?brokerlist='tcp://"+$scope.ip+":"+$scope.port+"'";
+
+    $scope.validateURL = function(url, flag) {
+        
+        console.log("Check");
+        var url = url;
+
+        var regex = /^amqp:\/\/[\w.]+:[\w.]+@clientid\/carbon\?brokerlist='tcp:\/\/[\w.]+:[0-9]+'/;
+
+        //The above regex is not perfect. There is a lot to edit
+
+        if (!regex.test(url)) { 
+            if (flag === "create"){
+                $scope.urlCreateValid = false;
+            } else if (flag === "check"){
+                $scope.urlCheckvalid = false;
+            }
+            $scope.valid = false;
+            return false;
+        }
+    
+        alert("URL is valid!!!");
+    };
+});
