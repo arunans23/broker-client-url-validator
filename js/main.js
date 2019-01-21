d5e8541d8600fc1865b5b3a9fc790200d8dd1591
Arunan Sugunakumar <arunans23@gmail.com>
2019-01-21 12:14:57 +0530

    Fix a bug in regex

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ app.controller('urlValidator', function($scope) {
         console.log("Check");
         var url = url;
 
-        var regex = /^amqp:\/\/[\w.]+:[\w.]+@clientid\/carbon\?brokerlist='tcp:\/\/[\w.]+:[0-9]+'/;
+        var regex = /^amqp:\/\/[\w.]+:[\w.]+@clientID\/carbon\?brokerlist='tcp:\/\/[\w.]+:[0-9]+'/;
 
         //The above regex is not perfect. There is a lot to edit
 
